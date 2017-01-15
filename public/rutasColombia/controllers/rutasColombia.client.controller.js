e5cd91544b2a2d938c13a2187c6fc3b81757904f
Jeisson Sanchez <jeisson_96@hotmail.com>
2017-01-15 09:52:49 -0500

    mapa

--- public/rutasColombia/controllers/rutasColombia.client.controller.js
+++ public/rutasColombia/controllers/rutasColombia.client.controller.js
@@ -1,69 +1,66 @@
 angular.module('rutasColombia').controller('rutasColombiaMapaCtrl', ['$scope',
-            function($scope) {
-                $scope.buscar = function() {
-                    //funcion por el cual se calcula el medio de transporte
-                    var calcularMedTransporte = new google.maps.DirectionsService;
-                    // Funcion que sirve para mostrar la ruta
-                    var mostrarMedTransporte = new google.maps.DirectionsRenderer;
-
-                    //Convierte el destino y origen en coordendas
-                    var geocoder = new google.maps.Geocoder(); //convierte el origen y destino en coordendas
-
-                    //genera un mapa en determina div
-                    var mapa = new google.maps.Map(document.getElementById('mapa'), {
-                        zoom: 6,
-                        center: {
-                            lat: 4.776735,
-                            lng: -74.166030
-                        }
-                    });
-                    
-                    mostrarMedTransporte.setMap(mapa);
-                    var calcularRuta = function() {
-                        calcularRuta(calcularMedTransporte, mostrarMedTransporte);
-                    };
-
-                    geocoder = new google.maps.Geocoder();
-                    $scope.buscar = function() {
-                        geocodeAddress(geocoder, mapa); //Da formato y geolocaliza
-                    };
-                    //document.getElementById('origen').addEventListener('change', onChangeHandler);
-                    //document.getElementById('Destino').addEventListener('change', onChangeHandler);
-
-                    //directionsDisplay.setMap(map);
-
-
-                    function calcularRuta(calcularMedTransporte, mostrarMedTransporte) {
-                        calcularMedTransporte.route({
-                            origin: $scope.origen, //document.getElementById('origen').value,
-                            destination: $scope.destino,
-                            travelMode: google.maps.TravelMode.DRIVING
-
-                        }, function(respuesta, estado) {
-                            if (estado === google.maps.DirectionsStatus.OK) {
-                                mostrarMedTransporte.setDirections(respuesta);
-                            } else {
-                                // window.alert('Directions request failed due to ' + status);
-                            }
-                        });
-                    }
-                    $scope.buscar = function() {
-                        geocodeAddress(geocoder, map);
-                    };
-
-                }]
-        }; -
-        angular.module('rutasColombia').controller('rutasColombiaBarCtrl', ['$scope',
-            function($scope) {
-                $scope.name = 'aplicacion Mean';
-            }
-        ]);
-
-        angular.module('rutasColombia').controller('rutasColombiaHomeCtrl', ['$scope',
-            function($scope) {
-                $scope.jei = function() {
-                    console.log($scope.origen);
+    function($scope) {
+        $scope.inicializarMapa = function() {
+            //funcion por el cual se calcula el medio de transporte
+            var calcularMedTransporte = new google.maps.DirectionsService;
+            // Funcion que sirve para mostrar la ruta
+            var mostrarMedTransporte = new google.maps.DirectionsRenderer;
+            //genera un mapa en determina div
+            var mapa = new google.maps.Map(document.getElementById('mapa'), {
+                zoom: 6,
+                center: {
+                    lat: 4.776735,
+                    lng: -74.166030
                 }
+            });
 
-            }
-        ]);
+            mostrarMedTransporte.setMap(mapa);
+
+            var llamadaCalcularRuta = function() {
+                calcularRuta(calcularMedTransporte, mostrarMedTransporte);
+            };
+
+            //Convierte el destino y origen en coordendas
+            var geocoder = new google.maps.Geocoder()
+            $scope.buscar = function() {
+                geocodeAddress(geocoder, mapa); //Da formato y geolocaliza
+            };
+        }
+
+        function calcularRuta(calcularMedTransporte, mostrarMedTransporte) {
+            calcularMedTransporte.route({
+                origin: $scope.origen, //document.getElementById('origen').value,
+                destination: $scope.destino,
+                travelMode: google.maps.TravelMode.DRIVING
+
+            }, function(respuesta, estado) {
+                if (estado === google.maps.DirectionsStatus.OK) {
+                    mostrarMedTransporte.setDirections(respuesta);
+                } else {
+                    // window.alert('Directions request failed due to ' + status);
+                }
+            });
+        }
+        $scope.buscar = function() {
+            geocodeAddress(geocoder, map);
+        };
+
+    }
+
+]);
+
+
+angular.module('rutasColombia').controller('rutasColombiaBarCtrl', ['$scope',
+    function($scope) {
+        $scope.name = 'aplicacion Mean';
+    }
+]);
+
+angular.module('rutasColombia').controller('rutasColombiaHomeCtrl', ['$scope',
+    function($scope) {
+        $scope.jei = function() {
+            console.log($scope.origen);
+        }
+
+    }
+]);
