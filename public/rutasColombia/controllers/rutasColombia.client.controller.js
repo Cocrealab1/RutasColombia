42eaa99196bb3f37558669c361f5cb3800e2ccdb
eimarv <eimarvelasquez96@gmail.com>
2017-01-16 21:13:58 -0500

    mapa json

--- public/rutasColombia/controllers/rutasColombia.client.controller.js
+++ public/rutasColombia/controllers/rutasColombia.client.controller.js
@@ -1,9 +1,20 @@
 angular.module('rutasColombia').controller('rutasColombiaHomeCtrl', ['$scope',
-  function($scope){
-    $scope.name = 'aplicacion Mean';
-  }]);
+    function($scope) {
+        $scope.name = 'aplicacion Mean';
+    }
+]);
 
-  angular.module('rutasColombia').controller('rutasColombiaBarCtrl', ['$scope',
-    function($scope){
-      $scope.name = 'aplicacion Mean';
-    }]);
+angular.module('rutasColombia').controller('rutasColombiaBarCtrl', ['$scope',
+    function($scope) {
+        $scope.name = 'aplicacion Mean';
+    }
+]);
+
+angular.module('rutasColombia').controller('geoCtrl', ['$scope', '$http',
+    function($scope, $http) {
+        $http.get('rutasColombia/json/geo.json')
+            .then(function(res) {
+                $scope.datos = res.data;
+            });
+    }
+]);
