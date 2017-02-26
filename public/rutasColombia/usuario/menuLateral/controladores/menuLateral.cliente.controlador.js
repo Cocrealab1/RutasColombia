10015718a0515a7e181ce7e727b6b0d4381a4783
Jeisson Sanchez <jeisson_96@hotmail.com>
2017-02-26 16:28:45 -0500

    Servicios

--- public/rutasColombia/usuario/menuLateral/controladores/menuLateral.cliente.controlador.js
+++ public/rutasColombia/usuario/menuLateral/controladores/menuLateral.cliente.controlador.js
@@ -1,3 +1,21 @@
-angular.module('menuLateral').controller('MenuLateralCtrl', ['$scope', '$http',
-    function($scope, $http) {}
+angular.module('menuLateral').controller('MenuLateralCtrl', ['$scope', '$http', 'MenuLateral',
+    function($scope, $http, MenuLateral) {
+
+        //$scope.usuario = MenuSuperior.registrar;
+
+        $scope.agregarImagen = function() {
+            MenuSuperior.add({
+                titulo: $scope.imagen.titulo,
+                telefono: $scope.imagen.telefono,
+                descripcion: $scope.imagen.descripcion,
+                donde: $scope.imagen.donde,
+                tipo: $scope.imagen.tipo
+            })
+            $scope.imagen.titulo = '';
+            $scope.imagen.telefono = '';
+            $scope.imagen.descripcion = '';
+            $scope.imagen.donde = '';
+            $scope.imagen.tipo = '';
+        }
+    }
 ]);
