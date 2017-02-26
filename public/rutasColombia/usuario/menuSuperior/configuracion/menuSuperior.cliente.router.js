b06616dea6eee89d156de253f54191a0eb4a5ec2
dafp18 <soporte_sistemas_zfb@ZFBDIROPE05.piciz.com>
2017-02-25 21:55:41 -0500

    Actualizacion para mi GIT
    Volvi  copiar los archivos eliminados a mi carpeta en el Github

---
+++ public/rutasColombia/usuario/menuSuperior/configuracion/menuSuperior.cliente.router.js
@@ -0,0 +1,20 @@
+/*angular.module('menuSuperior').config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        //$urlRouterProvider.otherwise("/menuSuperior");
+        $stateProvider
+            .state('menuSuperior', {
+                url: '/menuSuperior',
+                templateUrl: 'rutasColombia/usuario/menuSuperior/vistas/menuSuperior.cliente.vista.html'
+            })
+
+            .state('menuSuperior.registro', {
+                url: '/registro',
+                templateUrl: 'rutasColombia/usuario/menuSuperior/vistas/formularios/registro.cliente.vista.html'
+            })
+
+            .state('menuSuperior.ingresar', {
+                url: '/ingresar',
+                templateUrl: 'rutasColombia/usuario/menuSuperior/vistas/formularios/ingresar.cliente.vista.html'
+            });
+    }
+]);*/
