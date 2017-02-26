e6a6e9e2d7681756404f06972413d32273b0f1bb
Jeisson Sanchez <jeisson_96@hotmail.com>
2017-02-26 14:45:30 -0500

    Cluster

--- public/rutasColombia/usuario/mapa/controladores/mapa.cliente.controlador.js
+++ public/rutasColombia/usuario/mapa/controladores/mapa.cliente.controlador.js
@@ -5,7 +5,8 @@ angular.module('mapa').controller('MapaCtrl', ['$scope', '$http',
             .then(function(respuesta) {
                 var ventanaInfo;
                 var marcador;
-                
+                var marcadores = [];
+
                 for (var i = 0; i < respuesta.data.length; i++) {
 
                     /*Creacion de ventanas emergentes*/
@@ -21,6 +22,7 @@ angular.module('mapa').controller('MapaCtrl', ['$scope', '$http',
                         position: new google.maps.LatLng(respuesta.data[i].Coordenadas.lat, respuesta.data[i].Coordenadas.lng),
                         map: mapa
                     });
+                    marcadores.push(marcador);
 
                     /*Asignar evento click para mostrar el contenido*/
                     google.maps.event.addListener(marcador, 'click', (function(marcador, i) {
@@ -29,6 +31,12 @@ angular.module('mapa').controller('MapaCtrl', ['$scope', '$http',
                         }
                     })(marcador, i));
                 }
+                // Añade un clusterizador de marcadores para administrar los marcadores.
+                var clusterMarcadores = new MarkerClusterer(mapa, marcadores, {
+                    //la imagePath nos permite cambiar la imagen del markerclusterer se puede colocar una direccion web o ruta de donde est la imagen
+                    //imagePath: 'http://chart.apis.google.com/chart?cht=mm&chs=24x32&%27%20+%20%27chco=FFFFFF,008CFF,000000&ext=.png'
+                    imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+                });
             });
     }
 ]);
