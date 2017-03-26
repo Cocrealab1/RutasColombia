d2ccb6016d9aee1bd8c39a3002db303ed464e9e9
dafp18 <soporte_sistemas_zfb@ZFBDIROPE05.piciz.com>
2017-03-25 22:16:53 -0500

    Se agregan botones de Geolocalizacion
    Se colocan los botones de geolocalizacion en el menu seleccionar
    vehiculo estos aparecen al dar click sobre las areas de texto del punto
    A y el punto B.

--- public/rutasColombia/usuario/menuLateral/controladores/menuLateral.cliente.controlador.js
+++ public/rutasColombia/usuario/menuLateral/controladores/menuLateral.cliente.controlador.js
@@ -3,6 +3,28 @@ angular.module('menuLateral').controller('MenuLateralCtrl', ['$scope', '$http', 
 
         //$scope.usuario = MenuSuperior.registrar;
 
+        $BtnUbicActual=false;   // este boton aparece en el menu seleccionar vehiculo campo "punto A"
+        $BtnUbicActual2=false;  // este boton aparece en el menu seleccionar vehiculo campo "punto B"
+        $BtnUbicActual3=false;  // este boton es el que va a aprecer en el menu subir imagenes, campo "donde tomaste la foto"
+
+        $scope.BtnUbicActualA= function() { 
+        
+        $scope.BtnUbicActual=true;
+        $scope.BtnUbicActual2=false;
+        
+    }
+
+        $scope.BtnUbicAactualD= function() { 
+        
+        $scope.BtnUbicActual2=true;
+        $scope.BtnUbicActual=false;
+        
+    }
+        $scope.BtnUbicActualImg= function() { 
+        
+        $BtnUbicActual3=true;
+        
+    }
         $scope.agregarImagen = function() {
             MenuSuperior.add({
                 titulo: $scope.imagen.titulo,
