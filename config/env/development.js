726a26af8557be2beac31a454121322f33714d1a
jakeson123 <jeisson_96@hotmail.com>
2017-04-25 23:59:17 -0500

    jei

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
 module.exports = {
   //cambiar puesto que es un palabra secreta
   //db: 'mongodb://localhost/rutas',
-  db: 'mongodb://rutascolombia:dbrutas123@ds161029.mlab.com:61029/rutascolombia'
+  db: 'mongodb://rutascolombia:dbrutas123@ds161029.mlab.com:61029/rutascolombia',
   sessionSecreta: 'developmentSessionSecret'
 
 };
