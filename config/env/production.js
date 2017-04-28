659b0fc229361280eafae0bbaa988ce7ece7f60c
jakeson123 <jeisson_96@hotmail.com>
2017-04-27 21:38:19 -0500

    j

---
+++ config/env/production.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  //cambiar puesto que es un palabra secreta
+  db: 'mongodb://rutas:123456789@ds117819.mlab.com:17819/dbrutas'|| 'mongodb://localhost/munum',,
+  sessionSecreta: 'developmentSessionSecret'
+};
+
+
+/*URI GENERICA DE MONGO
+mongodb://nombreusuario:password@hostname:puerto/basedatos
+
+URI EN INSTANCIA localhost
+mongodb://localhost/mean*/
