a269a29790133c415f9d6b17a65fa8a22552ab76
jakeson123 <jeisson_96@hotmail.com>
2017-04-27 20:57:54 -0500

    favicon

--- public/rutasColombia/usuario/mapa/controladores/mapa.cliente.controlador.js
+++ public/rutasColombia/usuario/mapa/controladores/mapa.cliente.controlador.js
@@ -1,95 +1,102 @@
 angular.module('mapa').controller('MapaCtrl', ['$scope', '$http',
-    function($scope, $http) {
+  function($scope, $http) {
+    //Marcadores
+    $http.get('rutasColombia/usuario/mapa/json/peajes.json')
+      .then(function(respuesta) {
+        var ventanaInfo;
+        var marcador;
+        var marcadores = [];
 
-        $http.get('rutasColombia/usuario/mapa/json/peajes.json')
+        for (var i = 0; i < respuesta.data.length; i++) {
+
+          /*Creacion de ventanas emergentes*/
+          $http.get('rutasColombia/usuario/mapa/vistas/vetanaInfo.cliente.vista.html')
             .then(function(respuesta) {
-                var ventanaInfo;
-                var marcador;
-                var marcadores = [];
+              ventanaInfo = new google.maps.InfoWindow({
+                content: respuesta.data
+              });
+            })
 
-                for (var i = 0; i < respuesta.data.length; i++) {
+          /*Creacion de marcadores*/
+          marcador = new google.maps.Marker({
+            position: new google.maps.LatLng(respuesta.data[i].coordenadas.lat, respuesta.data[i].coordenadas.lng),
 
-                    /*Creacion de ventanas emergentes*/
-                    $http.get('rutasColombia/usuario/mapa/vistas/vetanaInfo.cliente.vista.html')
-                        .then(function(respuesta) {
-                            ventanaInfo = new google.maps.InfoWindow({
-                                content: respuesta.data
-                            });
-                        })
+            map: mapa,
+            icon: 'rutasColombia/usuario/mapa/img/apuntadorA.png'
+          });
+          marcadores.push(marcador);
 
-                    /*Creacion de marcadores*/
-                    marcador = new google.maps.Marker({
-                        position: new google.maps.LatLng(respuesta.data[i].coordenadas.lat, respuesta.data[i].coordenadas.lng),
-
-                        map: mapa,
-                        icon: 'rutasColombia/usuario/mapa/img/apuntadorA.png'
-                    });
-                    marcadores.push(marcador);
-
-                    /*Asignar evento click para mostrar el contenido*/
-                    google.maps.event.addListener(marcador, 'click', (function(marcador, i) {
-                        return function() {
-                            ventanaInfo.open(mapa, marcador);
-                        }
-                    })(marcador, i));
-                }
-                // Añade un clusterizador de marcadores para administrar los marcadores.
-                var clusterMarcadores = new MarkerClusterer(mapa, marcadores, {
-                    //la imagePath nos permite cambiar la imagen del markerclusterer se puede colocar una direccion web o ruta de donde est la imagen
-                    //imagePath: 'http://chart.apis.google.com/chart?cht=mm&chs=24x32&%27%20+%20%27chco=FFFFFF,008CFF,000000&ext=.png'
-                    imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-
-                });
+          /*Asignar evento click para mostrar el contenido*/
+          google.maps.event.addListener(marcador, 'click', (function(marcador, i) {
+            return function() {
+              ventanaInfo.open(mapa, marcador);
+            }
+          })(marcador, i));
+        }
+        // Añade un clusterizador de marcadores para administrar los marcadores.
+        var clusterMarcadores = new MarkerClusterer(mapa, marcadores, {
+          //la imagePath nos permite cambiar la imagen del markerclusterer se puede colocar una direccion web o ruta de donde est la imagen
+          //imagePath: 'http://chart.apis.google.com/chart?cht=mm&chs=24x32&%27%20+%20%27chco=FFFFFF,008CFF,000000&ext=.png'
+          imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+        });
 
 
-                //fondo gris mapa
-                  var colores = [
-                    {
-                        featureType: "geometry.fill",
-                        elementType: "geometry",
-                        stylers: [
-                            { saturation: -100 },
-                            { hue: "#373833" }
-                        ]
-                    },
-                    {
-                        featureType: 'water',
-                        stylers: [
-                            {color: '#1d334a'},
-                                {visibility: 'on'}
-                            ]
-                    },
-                    {
-                        featureType: 'landscape.natural.terrain',
-                        stylers: [
-                            {color: '#AFAFAF'}
-                            ]
-                    },
-                    {
-                        featureType: 'poi.attraction',
-                        featureType: 'poi.medical',
+    //personalizar mapa
+        var colores = [{
+            featureType: "geometry.fill",
+            elementType: "geometry",
+            stylers: [{
+                saturation: -100
+              },
+              {
+                hue: "#373833"
+              }
+            ]
+          },
+          {
+            featureType: 'water',
+            stylers: [{
+                color: '#1d334a'
+              },
+              {
+                visibility: 'on'
+              }
+            ]
+          },
+          {
+            featureType: 'landscape.natural.terrain',
+            stylers: [{
+              color: '#AFAFAF'
+            }]
+          },
+          {
+            featureType: 'poi.attraction',
+            featureType: 'poi.medical',
 
-                        stylers: [
-                            {color: '#0BB5C1'},
-                            {text: 'arial'}
-                            ]
-                    },
-                     {
-                        featureType: "transit",
-                        elementType: "geometry.stroke",
-                        elementType: "labels.text.stroke",
-                        stylers: [
-                            {color: '#E3C83A'}
-                            ]
-                    }
+            stylers: [{
+                color: '#0BB5C1'
+              },
+              {
+                text: 'arial'
+              }
+            ]
+          },
+          {
+            featureType: "transit",
+            elementType: "geometry.stroke",
+            elementType: "labels.text.stroke",
+            stylers: [{
+              color: '#E3C83A'
+            }]
+          }
 
-                ];
+        ];
 
-                var estilo = new google.maps.StyledMapType(colores);
-                mapa.mapTypes.set('mapa-bn', estilo);
-                mapa.setMapTypeId('mapa-bn');
+        var estilo = new google.maps.StyledMapType(colores);
+        mapa.mapTypes.set('mapa-bn', estilo);
+        mapa.setMapTypeId('mapa-bn');
 
-                //google.maps.event.addDomListener(window, 'onload', inicializarMapa);
-            });
-    }
+        //google.maps.event.addDomListener(window, 'onload', inicializarMapa);
+      });
+  }
 ]);
